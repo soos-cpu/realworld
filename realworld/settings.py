a9974dec4b70156f9bc64f8e798de27fde1019f8
unknown <2499872644@qq.com>
2024-01-18 10:37:23 +0800

    [IMP]

--- realworld/settings.py
+++ realworld/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = "realworld.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        'NAME': 'django',
+        'USER': 'tokyo',
+        'PASSWORD': 'kinkasei@pg000LDJ999ldj',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
