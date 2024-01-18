4f46296386bb7bcea9e58568b9aebebb00c22479
unknown <2499872644@qq.com>
2024-01-18 10:40:32 +0800

    [IMP]

--- realworld/settings.py
+++ realworld/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-f35(x7w#1hz7%oejc(t(x8ii7n^%n0pvzsp@x*qtfh8^$3^3j+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
